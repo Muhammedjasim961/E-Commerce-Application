a30b1baaf212413577718407499b3d847e565d16
Muhammedjasim961 <jasimwayanad961@gmail.com>
2025-03-25 00:38:20 +0530

    update changes

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/E-Commerce-Application",
+  base: "/E-Commerce-Application/",
 });
